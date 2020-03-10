5a00ab0c81cb0308e4533f06143b9d5f2dd6b571
WilliamDWesp <48689538+WilliamDWesp@users.noreply.github.com>
2020-03-09 20:55:35 -0500

    Added states input

---
+++ scripts/fill_Lists.js
@@ -0,0 +1,18 @@
+function getStates() {
+    var url, stateOptions;
+    url = '/states.json';
+
+
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(json=>{
+
+        Object.keys(json).forEach(function(key){
+
+            let option = document.createElement('option');
+            option.value = json[key].abbreviation; 
+            option.innerHTML=json[key].name;
+            document.getElementById("states[]").appendChild(option);
+        });//end of loop
+    })//end of then
+}//end of getStates
