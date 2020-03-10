9f6c38f672ee1900c94b137ef301cf91a09e3138
WilliamDWesp <48689538+WilliamDWesp@users.noreply.github.com>
2020-03-09 21:26:39 -0500

    added jsons+built the cities

--- scripts/fill_Lists.js
+++ scripts/fill_Lists.js
@@ -1,6 +1,6 @@
 function getStates() {
     var url, stateOptions;
-    url = '/states.json';
+    url = '/jsonFiles/states.json';
 
 
     fetch(url)
@@ -20,6 +20,29 @@ function getStates() {
 
 function showAndFillCities(){
     document.getElementById("cities").hidden = false;
+    var x = document.getElementById("StatesID");
+    var url='/jsonFiles/'+x.value+'.json';
+    
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(json=>{
 
+        Object.keys(json).forEach(function(key){
+            //console.log(json[key]);
+            let option = document.createElement('option');
+            option.value = json[key].city; 
+            option.innerHTML=json[key].city;
+            document.getElementById("cities[]").appendChild(option);
+        });//end of loop
+    })//end of then
+    
+}//end of showAndFillCities
 
-}
+function changeToDisplay(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        value: value
+    }));
+}//end of changeToDisplay
