03e21e5f25a24498f5d79aaff7c4a595b97c9d57
WilliamDWesp <48689538+WilliamDWesp@users.noreply.github.com>
2020-03-09 22:43:40 -0500

    starting to parse api

--- scripts/fill_Lists.js
+++ scripts/fill_Lists.js
@@ -10,8 +10,7 @@ function getStates() {
         Object.keys(json).forEach(function(key){
 
             let option = document.createElement('option');
-            option.value = json[key].abbreviation; 
-            option.innerHTML=json[key].name;
+            option.value = json[key].abbreviation+"|"+json[key].name; 
             document.getElementById("states[]").appendChild(option);
         });//end of loop
     })//end of then
@@ -21,7 +20,8 @@ function getStates() {
 function showAndFillCities(){
     document.getElementById("cities").hidden = false;
     var x = document.getElementById("StatesID");
-    var url='/jsonFiles/'+x.value+'.json';
+    var x2=x.value.split("|");
+    var url='/jsonFiles/'+x2[0]+'.json';
     
     fetch(url)
     .then(Response=>Response.json())
@@ -41,5 +41,46 @@ function showAndFillCities(){
 function changeToDisplay(){
     var x= document.getElementById("StatesID");
     var y= document.getElementById("city");
+
+
     window.location.href = "displayWeather?state="+x.value+"&city="+y.value;
 }//end of changeToDisplay
+
+
+function fromUrlMakeChange(){
+    var vars = {};
+    var url = window.location.href;
+    var parts= url.split("?");
+    var args = parts[1].split("&");
+    var state= args[0].split("=");
+    var city=args[1].split("=");
+
+    var stateV2=state[1].split("|");
+    
+    document.getElementById("state").innerHTML=stateV2[1];
+    document.getElementById("city").innerHTML=city[1];
+
+}
+
+function getLocationWeather(){
+    var state=document.getElementById("state").innerHTML;
+    var city=document.getElementById("city").innerHTML;
+    var Main= document.getElementById("Main");
+    var Maindesc= document.getElementById("Maindesc");
+    var temp= document.getElementById("temp");
+    var feels= document.getElementById("feels");
+    var wind= document.getElementById("wind");
+    var windDir= document.getElementById("windDir");
+    var cord= document.getElementById("cord");
+
+    var url="https://api.openweathermap.org/data/2.5/weather?q="+city+","+state+"&appid=6b752666aa5b10b0e7f8bb437aa0ed95";
+    console.log(url);
+    
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(Response=>{
+        cord.innerHTML=JSON.stringify(Response.coord)
+        
+    })
+
+}
